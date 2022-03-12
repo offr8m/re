d83dbfaa901dafc690c2d26b846bb1c171f72778
offr8m <100024343+offr8m@users.noreply.github.com>
2022-03-12 23:39:33 +0900

    ㅇ

--- blog/src/hooks/msgs/useMsgs.tsx
+++
@@ -1,7 +0,0 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../../modules";
-
-export default function useMsgs() {
-  const msgs = useSelector((state: RootState) => state.msgs);
-  return msgs;
-}
